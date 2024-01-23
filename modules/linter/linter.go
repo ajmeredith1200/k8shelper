e5c46c966467dfb6abd04d9a543ab59df533d051
alex meredith <alexanderm@packt.com>
2024-01-23 20:35:52 +0000

    started on a basic linter

--- modules/linter/linter.go
+++ modules/linter/linter.go
@@ -1,9 +1,27 @@
 package linter
 
-func init(){
-  //TODO
-}
+import (
+  "fmt"
+)
 
-func linter(){
-  //TODO
+func init(){
+  var path string
+  kubeManifestFiles := make(map[string]string)
+  fmt.Println("Manifests path: ")
+  fmt.Scan(&path)
+  entries, err := os.readDir(path)
+  if err != nil{
+    fmt.Errorf("Path Not Found")
+  }
+  for _, err in range kubeManifestFiles{
+    counter := 0
+    linter(kubeManifestFiles[counter])
+    counter ++
+  }
+}
+//TODO this wont work ^ but cant test readDir output on this laptop, 
+// you need to get each path as its own index in the map then you can iterate over each 
+//manifests at the start of the linter.
+func linter(kubeManifestFiles){
+  
 }
